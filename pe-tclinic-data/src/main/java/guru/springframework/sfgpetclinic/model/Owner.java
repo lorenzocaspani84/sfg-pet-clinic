bdfbb2e37d47a8664024e36a935e8d58b02e3b46
Lorenzo Caspani <caspani.lorenzo@gmail.com>
2018-11-10 18:19:07 +0100

    added vistis, updated owner with pet relationship. Closes #26

--- pe-tclinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pe-tclinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by lorenzo on 29/10/18.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
