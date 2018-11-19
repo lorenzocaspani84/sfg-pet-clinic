b7e3bbb373223efdf00bf44631184c58e3567007
Lorenzo Caspani <caspani.lorenzo@gmail.com>
2018-11-19 23:05:34 +0100

    Closes #27

--- pe-tclinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pe-tclinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by lorenzo on 29/10/18.
  */
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
