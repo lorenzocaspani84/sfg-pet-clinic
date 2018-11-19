b247424b993af0f74282bc801ad5ee57c990d9d2
Lorenzo Caspani <caspani.lorenzo@gmail.com>
2018-11-19 23:51:49 +0100

    Closes #31

---
+++ pe-tclinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by lorenzo on 19/11/18.
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pe-tclinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,38 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by lorenzo on 19/11/18.
+ */
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality objec) {
+        super.delete(objec);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
